49676bd511ecfbc8bbf4777a8fc9ba4f09384130
Ravis96 <ravis96@spoko.pl>
2024-03-30 15:18:02 +0100

    Test complete, release 2.0-beta.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "cc.dreamcode.command"
-    version = "2.0-pre.3"
+    version = "2.0-beta.1"
 
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
