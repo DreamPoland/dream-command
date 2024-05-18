268c02db23c8a97782b7800fcc7a2aaf28fbb329
Ravis96 <ravis96@spoko.pl>
2024-05-18 13:10:22 +0200

    Bump dream-utilities to 1.4.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     // -- dream-utilities --
-    implementation("cc.dreamcode:utilities:1.3.3")
+    implementation("cc.dreamcode:utilities:1.4.1")
 
     // -- tests --
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
